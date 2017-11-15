af39a47a58bf8a4e6fd7825f9ae7d40519248cfa
xsentinal <32465752+xsentinal@users.noreply.github.com>
2017-11-15 17:45:21 +0100

    Add plane detection.

--- Hoops/ViewController.swift
+++ Hoops/ViewController.swift
@@ -9,21 +9,35 @@
 import UIKit
 import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
 
+    @IBOutlet weak var planeDetected: UILabel!
     @IBOutlet weak var sceneView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.configuration.planeDetection = .horizontal
         self.sceneView.session.run(configuration)
         self.sceneView.autoenablesDefaultLighting = true
+        self.sceneView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARPlaneAnchor else {return}
+        DispatchQueue.main.async {
+             self.planeDetected.isHidden = false
+        }
+        //This hiddes the label after 3 seconds
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.planeDetected.isHidden = true
+        }
     }
 
 

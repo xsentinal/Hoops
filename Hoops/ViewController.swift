9c0a14305c47bd96d383d9a3e640dda52e132f2e
xsentinal <32465752+xsentinal@users.noreply.github.com>
2017-11-16 09:22:52 +0100

    Complete project

--- Hoops/ViewController.swift
+++ Hoops/ViewController.swift
@@ -8,19 +8,19 @@
 
 import UIKit
 import ARKit
+import Each
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet weak var planeDetected: UILabel!
     @IBOutlet weak var sceneView: ARSCNView!
     //The force applied to the ball
-    var power:Float = 1.0
+    var power:Float = 1
+    var timer = Each(0.05).seconds
     let configuration = ARWorldTrackingConfiguration()
     
     //Function checks is there is a basket ball net in out sceneView
-    var basketAdded: Bool {
-        return self.sceneView.scene.rootNode.childNode(withName: "Basket", recursively: false) != nil
-    }
+    var basketAdded: Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
         self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
@@ -31,26 +31,45 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         //Tap recognizer
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
         self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+        tapGestureRecognizer.cancelsTouchesInView = false
         
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         // Check if there is a basketball net
         if self.basketAdded == true {
+            timer.perform(closure: { () -> NextStep in
+                    self.power += 1
+                    return .continue
+            })
+           
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if self.basketAdded == true {
+            self.timer.stop()
+            self.shootBall()
+        }
+        self.power = 1
+    }
+        func shootBall() {
             guard let pointOfView = self.sceneView.pointOfView else {return}
-            self.power = 10
-            
+            self.removeEveryOtherBall()
             let transform = pointOfView.transform
             let location = SCNVector3(transform.m41, transform.m42, transform.m43)
             let orientation = SCNVector3(-transform.m31, -transform.m32, -transform.m33)
             let position = location + orientation
-            let ball = SCNNode(geometry: SCNSphere(radius: 0.3))
+            let ball = SCNNode(geometry: SCNSphere(radius: 0.2))
             ball.geometry?.firstMaterial?.diffuse.contents = "ball"
             ball.position = position
             let body = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: ball))
             ball.physicsBody = body
+            ball.name = "Basketball"
+            //The energy that is lost as the ball collids with the backboard
+            body.restitution = 0.2
             ball.physicsBody?.applyForce(SCNVector3(orientation.x*power, orientation.y*power, orientation.z*power), asImpulse: true)
             self.sceneView.scene.rootNode.addChildNode(ball)
-        }
+    
     }
     // Objective C function
     @objc func handleTap(sender: UITapGestureRecognizer){
@@ -66,6 +85,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     func addBasket(hitTestResult: ARHitTestResult){
+        if basketAdded == false {
         let basketScene = SCNScene(named: "Basketball.scnassets/Basketball.scn")
         let basketNode = basketScene?.rootNode.childNode(withName: "Basket", recursively: false)
         let positionOfPlane = hitTestResult.worldTransform.columns.3
@@ -73,8 +93,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let yPosition = positionOfPlane.y
         let zPosition = positionOfPlane.z
         basketNode?.position = SCNVector3(xPosition, yPosition, zPosition)
-        basketNode?.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(node: basketNode!, options: [SCNPhysicsShape.Option.keepAsCompound: true, SCNPhysicsShape.Option.type: SCNPhysicsShape.ShapeType.concavePolyhedron]))
-        self.sceneView.scene.rootNode.addChildNode(basketNode!)
+       // Detection of objects with higher accuracy
+            basketNode?.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(node: basketNode!, options: [SCNPhysicsShape.Option.keepAsCompound: true, SCNPhysicsShape.Option.type: SCNPhysicsShape.ShapeType.concavePolyhedron]))
+            self.sceneView.scene.rootNode.addChildNode(basketNode!)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                self.basketAdded = true
+        }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -91,6 +116,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             self.planeDetected.isHidden = true
         }
+    }
+    func removeEveryOtherBall(){
+        self.sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+            if node.name == "Basketball" {
+             node.removeFromParentNode()
+            }
+        }
+    }
+    deinit {
+        self.timer.stop()
     }
 
 
---
+++ Pods/Target Support Files/Pods-Hoops/Pods-Hoops-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Hoops : NSObject
+@end
+@implementation PodsDummy_Pods_Hoops
+@end
